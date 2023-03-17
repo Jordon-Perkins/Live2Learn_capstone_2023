1082c572bc399649aa5b655aeb81ddab5dceed39
Jordon Perkins <jordon.rubel@gmail.com>
2023-03-17 16:19:26 -0500

    my classes work

--- src/managers/StudentManager.js
+++ src/managers/StudentManager.js
@@ -6,3 +6,12 @@ export const getClasses = () => {
     })
         .then(response => response.json())
 }
+
+export const getAttendingClasses = () => {
+    return fetch("http://localhost:8000/classes_attending", {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("l2l_token")}`
+        }
+    })
+        .then(response => response.json())
+}
